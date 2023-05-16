83cc690bfb61aed525fd303ff247485dca6261bb
Rui Menezes <ruizivo.menezes@gmail.com>
2023-05-16 14:51:01 -0300

    init repo

---
+++ configs/init-db.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE IF NOT EXISTS otel;
+
+/* CREATE VIEW trace_view AS
+SELECT
+	TraceId AS traceID,
+	SpanId AS spanID,
+	SpanName AS operationName,
+	ParentSpanId AS parentSpanID,
+	ServiceName AS serviceName,
+	Duration / 1000000 AS duration,
+	Timestamp AS startTime,
+	arrayMap(key -> map('key', key, 'value', SpanAttributes[key]), mapKeys(SpanAttributes)) AS tags,
+	arrayMap(key -> map('key', key, 'value', ResourceAttributes[key]), mapKeys(ResourceAttributes)) AS serviceTags
+FROM otel_traces
+WHERE TraceId = {trace_id:String}
+ */
